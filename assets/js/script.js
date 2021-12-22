87bb85d450447e062d705331029c9594719ca5f2
jackiem2 <jacqueline.merlan@gmail.com>
2021-12-21 19:52:38 -0600

    changed variable declaration

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
-var buttonE1 = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-buttonE1.addEventListener("click", function(){
+var createTaskHandler = function(event) {
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-});
+};
+
+formEl.addEventListener("submit", createTaskHandler);
